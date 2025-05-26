6d45ecbeaa1105e3584f73a9861e0d665e20a34f
koiwa <ikanohiza@gmail.com>
2025-05-26 17:01:16 +0900

    FindAllのルーティング追加

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"emotra-backend/api/models"
+	"emotra-backend/controllers"
+	"emotra-backend/repositories"
+	"emotra-backend/usecases"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	diaries := []models.Diary{
+		{ID: 1, UserID: 1, Date: "2025-05-01", Mental: 5, Diary: "今日はとても良い日でした。"},
+		{ID: 2, UserID: 1, Date: "2025-05-02", Mental: 3, Diary: "普通の日でした。"},
+		{ID: 3, UserID: 2, Date: "2025-05-01", Mental: 4, Diary: "少し疲れましたが、充実した日でした。"},
+	}
+
+	diaryRepository := repositories.NewDiaryMemoryRepository(diaries)
+	diaryUsecase := usecases.NewDiaryUsecase(diaryRepository)
+	diaryController := controllers.NewDiaryController(diaryUsecase)
+
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // 0.0.0.0:8080 でサーバーを立てます。
+	router.GET("/diaries", diaryController.FindAll)
+	router.Run()
 }
